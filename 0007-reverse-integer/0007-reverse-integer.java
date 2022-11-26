9eef777cbc2477d373d29f5e1a27ac579c10e4bb
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-11-26 14:11:10 -0600

    Time: 5 ms (14.47%), Space: 41.3 MB (52.94%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,9 +2,8 @@ class Solution {
     public int reverse(int x) {      
         long res = 0;
         while (x != 0) {
-            int num = x % 10; 
+            res = res*10 + x % 10; 
             x = x / 10;
-            res = res*10 + num;
         }
         if (res > -Math.pow(2,31) && res < Math.pow(2,31) - 1) {
             return (int) res;
