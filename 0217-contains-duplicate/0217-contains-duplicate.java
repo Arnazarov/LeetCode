951b7be008aaffd4955ce86e08ae0277ded1869d
Ovez Arnazarov <ala200bama@gmail.com>
2022-12-10 16:40:03 -0600

    Time: 5 ms (98.11%), Space: 50.7 MB (97.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                return true;
-            }
-            map.put(nums[i], i);
-        }
-
-        return false;
+        Set<Integer> s = new HashSet<Integer>();
+     for(int i :nums){
+         if(s.add(i)){
+             continue;
+         }
+         else {
+             return true;
+         }
+     }
+     return false;      
     }
 }
